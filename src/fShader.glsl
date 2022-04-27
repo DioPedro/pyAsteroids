ca8fc3294f100a3e41d0bf1c8d31447831f25529
Marco Toledo <marcoantonioribeirodetoledo@gmail.com>
2022-04-27 01:22:56 -0300

    fix: Removes lighting

--- src/fShader.glsl
+++ src/fShader.glsl
@@ -5,6 +5,5 @@ in vec2 wPos;
 out vec4 outColor;
 
 void main() {
-    // outColor = color * distance(vec4(wPos, 0.0, 1.0), vec4(0.0, 0.0, 0.0, 0.0));
-    outColor = color * 0.1;
+    outColor = color;
 }
